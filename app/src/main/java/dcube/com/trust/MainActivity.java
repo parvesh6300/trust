98f3dd9ec4ef9dc3fc2b7898e636bd706887329c
Yadwinder Singh <ryker1990@gmail.com>
2016-10-09 13:43:03 +0530

    test commit

--- app/src/main/java/dcube/com/trust/MainActivity.java
+++ app/src/main/java/dcube/com/trust/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    int a = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
