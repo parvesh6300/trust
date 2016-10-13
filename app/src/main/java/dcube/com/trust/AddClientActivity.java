8bde3fa36477743d96ef2f893b5568d3119655c9
Yadwinder Singh <ryker1990@gmail.com>
2016-10-13 13:10:37 +0530

    test commit

--- app/src/main/java/dcube/com/trust/AddClientActivity.java
+++ app/src/main/java/dcube/com/trust/AddClientActivity.java
@@ -1,24 +1,75 @@
 package dcube.com.trust;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class AddClientActivity extends Activity {
 
+    Context context;
+
     String[] spinnerArray = {"10-19","20-29","30-39","40+"};
     Spinner age_group;
+    TextView addclient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_client);
 
+        context = this;
         age_group = (Spinner) findViewById(R.id.age_group);
+        addclient = (TextView) findViewById(R.id.addclient);
 
-        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
-                android.R.layout.simple_spinner_dropdown_item, spinnerArray);
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerArray);
         age_group.setAdapter(spinnerArrayAdapter);
+
+        addclient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                CustomDialogClass cdd=new CustomDialogClass(AddClientActivity.this);
+                cdd.show();
+            }
+        });
+
+    }
+
+    public class CustomDialogClass extends Dialog{
+
+        public Activity c;
+        public Dialog d;
+        public Button ok;
+
+        public CustomDialogClass(Activity a) {
+            super(a);
+            // TODO Auto-generated constructor stub
+            this.c = a;
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.custom_dialog);
+            ok = (Button) findViewById(R.id.btn_yes);
+
+            ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
+        }
     }
 }
+
+
+
