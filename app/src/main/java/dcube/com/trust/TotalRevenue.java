89bd9c624d06e8a38e70e705cb04ee81b0dbb7ca
Yadwinder <ryker1990@gmail.com>
2016-10-17 15:52:37 +0530

    rohit

--- app/src/main/java/dcube/com/trust/TotalRevenue.java
+++ app/src/main/java/dcube/com/trust/TotalRevenue.java
@@ -7,7 +7,16 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
+
 public class TotalRevenue extends AppCompatActivity {
+
+
+    ArrayList<Double> time= new ArrayList<>();
+    ArrayList<Double> revenue= new ArrayList<>();
+
+    Double[] ar_time= new Double[]{};
+    Double[] ar_revenue= new Double[]{};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +27,50 @@ public class TotalRevenue extends AppCompatActivity {
         getSupportActionBar().hide();
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        addValuesInList();
+
+
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
 
 
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
+                new DataPoint(0, 0),
+                    new DataPoint(1, 5),
+                    new DataPoint(2, 3)
         });
         graph.addSeries(series);
 
 
+        for (int i=0;i<5;i++)
+        {
+
+        }
+
+
+
     }
+
+
+    public void addValuesInList()
+    {
+        time.add(1.0);
+        time.add(2.0);
+        time.add(3.0);
+        time.add(4.0);
+        time.add(5.0);
+
+
+        revenue.add(1000.0);
+        revenue.add(1550.0);
+        revenue.add(1330.0);
+        revenue.add(2530.0);
+        revenue.add(3000.0);
+
+        ar_time=time.toArray(ar_time);
+        ar_revenue= revenue.toArray(ar_revenue);
+
+
+    }
+
+
 }
--- app/src/main/java/dcube/com/trust/WithDrawMoneyActivity.java
+++ app/src/main/java/dcube/com/trust/WithDrawMoneyActivity.java
@@ -1,13 +1,96 @@
 package dcube.com.trust;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class WithDrawMoneyActivity extends AppCompatActivity {
+public class WithDrawMoneyActivity extends Activity {
+
+    RadioGroup radio_group;
+
+    RadioButton radio_other;
+
+    EditText ed_other;
+
+    TextView tv_withdraw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_with_draw_money);
+
+
+        radio_group=(RadioGroup)findViewById(R.id.radio_group);
+        radio_other=(RadioButton)findViewById(R.id.radio_other);
+
+        ed_other=(EditText)findViewById(R.id.ed_other);
+
+        tv_withdraw=(TextView)findViewById(R.id.tv_withdraw);
+
+
+        radio_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                if (radio_other.isChecked())
+                {
+                    ed_other.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    ed_other.setVisibility(View.INVISIBLE);
+                }
+
+
+            }
+        });
+
+
+        tv_withdraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+
     }
+
+
+    public class ConfirmDialog extends Dialog{
+
+        public ConfirmDialog(Context context) {
+            super(context);
+        }
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView();
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
